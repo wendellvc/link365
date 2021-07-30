ebafd62ff3cce677be813d2aab0709bc972b52b7
Wendell Cabalhin <w.cabalhin@arcanys.com>
2021-07-31 00:20:03 +0800

    Added AJAX functionality when updating status to paid, and filtering status

--- develop/js/link365.js
+++ develop/js/link365.js
@@ -10,101 +10,84 @@
 (function ($) {
  	'use strict'; 
 
-  var ppp = $('#posts_per_page').data('posts'); // Post per page
-  var cat = $('#catid').data('catid');
+  var ppp = $('#posts_per_page').data('ppp'); // Post per page
   var pageNumber = 1;
 
-  function load_posts(filter_by, values, load_more) {
-      var categories = values.join('_');
-      cat = ( filter_by == 'All' ? '' : categories );
-      pageNumber = ( load_more == true ? pageNumber++ : 0 );
-
-      pageNumber++;
-
-      var str = 'cat=' + cat + '&pageNumber=' + pageNumber + '&ppp=' + ppp + '&action=link365_load_more_post_ajax';
-
-      $.ajax({
-          type: "POST",
-          dataType: "html",
-          url: ajax_posts.ajaxurl,
-          data: str,
-          success: function(data) {
-
-              var $data = $(data);
-              if($data.length) {
-                if( pageNumber == 1 ) {
-                  $("#ajax_posts").html($data);
-                } else {
-                  $("#ajax_posts").append($data);
-                }
-
-                  $("#more_posts").attr("disabled", false);
-              } else{
-                  $("#more_posts").attr("disabled", 'disabled');
-                  $("#msg_notice").html('<div class="alert">No older posts found.</adiv>');
-              }
-          },
-          error : function(jqXHR, textStatus, errorThrown) {
-              console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
-          }
-
-      });
-  }
-
-  /*
-  ** Case Studies and testimonials
-  */
-  var post_type = $('#post_type').val();
-  $("#more_custom_posts").on("click", function(e) { // When btn is pressed.
-      e.preventDefault();
-      $(this).attr("disabled", 'disabled'); // Disable the button, temp.
-      // post_type = $('#post_type').val();
-      load_custom_posts('');
-  });
-
-  $('.opt_posts_toggle').on('click', function(e) {
-    /* setting an active/selected */
+  /**
+   * Filter status
+   */
+  $( ".btn-status-filter" ).on( 'click', function( e ) { // When btn is pressed.
     e.preventDefault();
-    $('.opt_posts_toggle').removeClass('active');
-    $(this).addClass('active');
-    post_type = $(this).find('input').val();
-    $('#post_type').val(post_type);
-    load_custom_posts(post_type);
-    $("#msg_notice").html('');
-  });
 
-  function load_custom_posts(type) {
-    pageNumber = ( type != '' ? 0 : pageNumber++ );
-    pageNumber++;
-    post_type = ( type == '' ? $('#post_type').val() : type );
-    var str = 'post_type=' + post_type + '&pageNumber=' + pageNumber + '&ppp=' + ppp + '&action=link365_load_more_post_ajax';
+    var status = $(this).data('status'); // get the filter status value
+
+    $(".btn-status-filter, .btn-status-filter-all").removeClass('active'); // remove the current active class to the status buttons
+    $(this).addClass('active'); // add active class to the button being clicked
+    
+    var str = 'ppp=' + ppp + '&status=' + status + '&action=link365_load_filter_status_post_ajax';
 
     $.ajax({
-        type: "POST",
-        dataType: "html",
-        url: ajax_posts.ajaxurl,
-        data: str,
-        success: function(data) {
+      type: "POST",
+      dataType: "html",
+      url: ajax_posts.ajaxurl,
+      data: str,
+      success: function(data) {
 
-            var $data = $(data);
-            if($data.length) {
-              if( pageNumber == 1 ) {
-                $("#ajax_posts").html($data);
-              } else {
-                $("#ajax_posts").append($data);
-              }
+        var $data = $(data);
+        $("#tbl_ajax").html($data);
+        
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+      }
 
-                $("#more_custom_posts").attr("disabled", false);
-            } else {
-              $("#more_custom_posts").attr("disabled", 'disabled');
-              $("#msg_notice").html('<div class="alert">No older posts found.</adiv>');
-            }
-        },
-        error : function(jqXHR, textStatus, errorThrown) {
-            console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
-        }
+    } );
 
+  });
+
+  /**
+   * Select/Deselect all checkboxes
+   */
+  $( "#checkbox_all" ).on( 'click', function( e ) { // When btn is pressed.
+    // e.preventDefault();
+    if( $( this ).is( ':checked' ) ) {
+      $('.checkbox_select').attr('checked', 'checked');
+    } else {
+      $('.checkbox_select').removeAttr('checked');
+    }
+  } );
+
+  
+  $( '#mark_as_paid' ).on( 'click', function( e ) { 
+    var arrayIDs = [];
+    $.each( $( "input.checkbox_select:checked" ), function (key, data) {   
+      var dValue =  $( this ).data( 'checkboxid' );
+      arrayIDs.push(dValue);
     });
-  }
+
+    var str = 'IDs=' + arrayIDs + '&action=link365_update_posts_status_ajax';
+
+    $.ajax({
+      type: "POST",
+      dataType: "html",
+      url: ajax_posts.ajaxurl,
+      data: str,
+      success: function(data) {
+        
+        var $data = $(data);
+        console.log($data);
+        $("#notification").html($data);
+
+        $.each( $( "input.checkbox_select:checked" ), function (key, data) {   
+          var dValue =  $( this ).data( 'checkboxid' );
+          $( '#status_update_' + dValue ).removeAttr('class').addClass('badge rounded-pill bg-info').text('PAID');
+        });
+      },
+      error : function(jqXHR, textStatus, errorThrown) {
+          console.log(jqXHR + " :: " + textStatus + " :: " + errorThrown);
+      }
+
+    } );
+  } );
 
 })(jQuery);
